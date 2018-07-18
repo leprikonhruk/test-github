5a3e6ea1f16f8cf60500bdc8c553eb5c770490b1
leprikonhruk <markid@mail.ru>
2018-07-18 23:20:48 +0300

    Initial commit

---
+++ www/config/db-local.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 18.07.2018
+ * Time: 13:36
+ */
+
+phpinfo();
---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+function dds(){
+	echo "!~~~~!~~~~!";
+	echo "098789";
+	return true;
+}
+
+echo "00006666000000";
+
+dds();
+
